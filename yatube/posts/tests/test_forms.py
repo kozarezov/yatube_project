705f086c9f59f6cb8d8cd48bf4bd6ed21e81d997
kozarezov <kozarezov.dd@gmail.com>
2022-07-04 14:04:22 +0300

    add image tests

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,12 +1,19 @@
+import shutil
+import tempfile
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from posts.models import Group, Post
 
 User = get_user_model()
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostFormTests(TestCase):
     """Тест view приложения posts."""
 
@@ -24,6 +31,25 @@ class PostFormTests(TestCase):
             text='Тестовый пост',
             group=cls.group
         )
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        cls.uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        """Удаляем директорию и всё её содержимое."""
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         """Создаем неавторизованного и авторизованного пользователя."""
@@ -71,3 +97,21 @@ class PostFormTests(TestCase):
         self.assertTrue(Post.objects.filter(text=form_fields['text']).
                         filter(group=form_fields['group']).
                         filter(author=self.user).exists())
+
+    def test_forms_image(self):
+        """Проверка добавления изображения."""
+        count_posts = Post.objects.count()
+        form_fields = {
+            'text': 'Тестовый пост 2',
+            'image': self.uploaded
+        }
+
+        response = self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_fields
+        )
+
+        self.assertTrue(Post.objects.filter(image='posts/small.gif').exists())
+        self.assertRedirects(response, reverse(
+                             'posts:profile', kwargs={'username': self.user}))
+        self.assertEqual(Post.objects.count(), count_posts + 1)
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,12 +1,18 @@
+from email.mime import image
+import shutil
+import tempfile
+
 from django import forms
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import Client, TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from posts.models import Group, Post
 
 User = get_user_model()
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
 class PostViewTests(TestCase):
@@ -15,7 +21,19 @@ class PostViewTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.post_create_url = reverse('posts:post_create')
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        cls.uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         cls.user = User.objects.create_user(username='auth')
         cls.user_1 = User.objects.create_user(username='auth_1')
         cls.group = Group.objects.create(
@@ -26,12 +44,15 @@ class PostViewTests(TestCase):
         cls.post = Post.objects.create(
             author=cls.user,
             text='Тестовый пост',
-            group=cls.group
+            group=cls.group,
+            image=cls.uploaded
         )
         cls.post_1 = Post.objects.create(
             author=cls.user_1,
             text='Тестовый пост 1',
+            image=cls.uploaded
         )
+        cls.post_create_url = reverse('posts:post_create')
         cls.index_url = reverse('posts:index')
         cls.group_url = reverse('posts:group_list',
                                 kwargs={'slug': cls.group.slug})
@@ -41,6 +62,11 @@ class PostViewTests(TestCase):
                                       kwargs={'post_id': '1'})
         cls.post_edit_url = reverse('posts:post_edit',
                                     kwargs={'post_id': '1'})
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
         """Создаем авторизованного пользователя."""
@@ -140,6 +166,23 @@ class PostViewTests(TestCase):
                 self.assertIsInstance(form_field, expected)
         self.assertEqual(response.context['post'].author, self.user)
         self.assertEqual(response.context['post'], self.post)
+
+    def test_views_context_with_image(self):
+        """Шаблоны c изображением сформированы с правильным контекстом."""
+        urls = {
+            self.index_url,
+            self.group_url,
+            self.profile_url,
+            self.post_detail_url
+        }
+
+        for url in urls:
+            with self.subTest(url=url):
+                response = self.authorized_client.get(url)
+                if response.context.get('object_list'):
+                    self.assertTrue(response.context['object_list'][0].image)
+                else:
+                    self.assertTrue(response.context['post'].image)
 
 
 class PostViewPaginatorTests(TestCase):
