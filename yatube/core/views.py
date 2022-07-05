9bc249133206706fd7d9fc98291353e71c5eb14f
kozarezov <kozarezov.dd@gmail.com>
2022-07-05 22:48:30 +0300

    add handler500

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -7,3 +7,11 @@ def csrf_failure(request, reason=''):
 
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
+
+
+def internal_server_error(request, exception):
+    return render(request, 'core/500.html', status=500)
+
+
+def permission_denied(request, exception):
+    return render(request, 'core/403.html', status=403)
