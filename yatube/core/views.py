0f96ca5eb8aac4f783018c39e86b22df642def5d
kozarezov <kozarezov.dd@gmail.com>
2022-11-07 23:39:05 +0400

    fix bug

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -9,7 +9,7 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
-def internal_server_error(request, exception):
+def internal_server_error(request):
     return render(request, 'core/500.html', status=500)
 
 
